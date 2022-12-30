9363582ad55b813181a346db4c10e55c1d878694
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2022-12-30 14:09:46 +0530

    Time: 16 ms (80.20%), Space: 14.3 MB (40.97%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> ans;
+        
+        helper(graph, ans, {0}, 0, graph.size());
+        
+        return ans;
+    }
+    
+    void helper(vector<vector<int>>& graph, vector<vector<int>>& ans, vector<int> tmp, int idx, int n){
+        if(idx==n-1){
+            ans.push_back(tmp);
+            return;
+        }
+        
+        for(int i=0;i<graph[idx].size();i++){
+            int num = graph[idx][i];
+            
+            tmp.push_back(num);
+            helper(graph, ans, tmp, num, n);
+            tmp.pop_back();
+        }
+    }
+};
