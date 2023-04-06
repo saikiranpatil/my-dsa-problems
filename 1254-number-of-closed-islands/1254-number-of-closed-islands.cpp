59ac93beb46b7c040b3277b34c79df326e768710
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-04-06 22:15:02 +0530

    Time: 34 ms (7.50%), Space: 18.3 MB (5.32%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int closedIsland(vector<vector<int>>& grid) {
-        vector<int> dx={-1,0,1,0}, dy={0,-1,0,1};
         int ans=0, m=grid.size(), n=grid[0].size();
+        vector<int> dx={-1,0,1,0}, dy={0,-1,0,1};
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         
         for(int i=1;i<m-1;i++){
             for(int j=1;j<n-1;j++){
-                if(grid[i][j]==0){
+                if(!visited[i][j] && grid[i][j]==0){
                     bool flag=true;
                     queue<int> qx, qy;
                     qx.push(i);
@@ -17,13 +18,13 @@ public:
                         qx.pop();
                         qy.pop();
                         
-                        grid[px][py]=2;
+                        visited[px][py]=true;
                         
                         if(px==0 || px==m-1 || py==0 || py==n-1) flag=false;
                         
                         for(int k=0;k<4;k++){
                             int nx=px+dx[k], ny=py+dy[k];
-                            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==0){
+                            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==0 && !visited[nx][ny]){
                                 qx.push(nx);
                                 qy.push(ny);
                             }
