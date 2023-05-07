d2cd18c532be9009679e97a366cc4a2e5e934aa6
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-05-07 10:12:04 +0530

    Time: 3 ms (70.23%), Space: 6.1 MB (21.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,9 +3,7 @@ public:
     int fib(int n) {
         vector<int> ans = {0,1};
         
-        for(int i=2;i<=n;i++){
-            ans.push_back(ans[i-1] + ans[i-2]);
-        }
+        for(int i=2;i<=n;i++) ans.push_back(ans[i-1] + ans[i-2]);
         
         return(ans[n]);
     }
