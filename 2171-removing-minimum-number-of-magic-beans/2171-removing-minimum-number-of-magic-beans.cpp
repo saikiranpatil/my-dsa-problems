526548b852b2ad9f4988177b69c4b9c1519560e0
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-05-24 13:19:34 +0530

    Time: 278 ms (35.05%), Space: 100.7 MB (93.47%) - LeetHub

--- 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     long long minimumRemoval(vector<int>& beans) {
-        vector<int> nums(beans.begin(), beans.end());
-        sort(nums.begin(), nums.end());
-        
-        long long n=nums.size(), sum=0, ans=0;
+        long long n=beans.size(), sum=0, ans=0;
+        sort(beans.begin(), beans.end());
         
         for(auto num: beans) sum+=num;
-        for(int i=0;i<n;i++) ans=max(ans, nums[i] * (n-i));
+        for(int i=0;i<n;i++) ans=max(ans, beans[i] * (n-i));
         
         return sum - ans;
     }
