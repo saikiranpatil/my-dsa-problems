bf9dddab21c211f8e15c4bf8d3cdb3c571ebc21f
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-05-27 19:28:00 +0530

    Time: 482 ms (33.77%), Space: 171 MB (7.28%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int n=nums.size(), ans=0;
+        int n=nums.size();
+        int ans=0;
         
         if(n==1) return ans;
         
         unordered_map<int, int> count_even, count_odd; 
-        int od=(n+1)/2, evn=n/2; //=// count of odd and even number in nums
-        
-        // cout<<od<<" "<<evn<<endl<<endl;
+        int od=(n+1)/2;
+        int evn=n/2;
         
         for(int i=0;i<n;i++){
             if(i%2==0) count_odd[nums[i]]++;
@@ -24,8 +24,6 @@ public:
         int ans12 = od - odd.first.second + evn - even.second.second;
         int ans21 = od - odd.second.second + evn - even.first.second;
         
-        // cout<<ans11<<" "<<ans12<<" "<<ans21<<endl;
-        
         if(odd.first.first != even.first.first) return ans11;
         if(odd.second.first==-1 && even.second.first==-1) return evn;
         if(odd.second.first==-1) return ans12;
@@ -35,8 +33,7 @@ public:
     }
     
     pair<pair<int, int>, pair<int, int>> helper(unordered_map<int, int> count_map){
-        pair<pair<int, int>, pair<int, int>> ans = {{-1, -1},{-1, -1}}; 
-        //=// ans = {{firstGreaterNum, firstGreaterCount}, {secondGreaterNum, secondGreaterCount}}
+        pair<pair<int, int>, pair<int, int>> ans = {{-1, -1},{-1, -1}};
         
         for(auto it: count_map){
             int count=it.second;
@@ -49,10 +46,6 @@ public:
                 ans.second = it;
             }
         }
-        
-        // cout<<ans.first.first<<" "<<ans.first.second<<endl;
-        // cout<<ans.second.first<<" "<<ans.second.second<<endl;        
-        // cout<<endl;
         
         return ans;
     }
