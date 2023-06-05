f0583867cc13beb42ee6d7711c583b379058047f
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-05 07:20:54 +0530

    Time: 11 ms (56.86%), Space: 10 MB (63.27%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -14,13 +14,22 @@ public:
         double m = ((y2-y1)*1.0)/(x2-x1);
         double c = y1 - m*x1;
         
+        if(x1==x2){
+            for(int i=2;i<n;i++){
+                
+                if(coordinates[i][0]!=x1) return false;
+            }
+            
+            return true;
+        }
+        
         for(int i=2;i<n;i++){
             int xn = coordinates[i][0];
             int yn = coordinates[i][1];
             
             double equation = yn - m*xn - c;
             
-            if((x1==x2 && coordinates[i][0]!=x1) || (x1!=x2 && equation != 0)) return false;
+            if(equation != 0) return false;
         }
         
         return true;
