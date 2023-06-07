72e0677484833f5a724fa8b38f8cc7bd6bbcb056
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-07 14:44:26 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (72.91%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    string capitalizeTitle(string s) {
-        for (int i = 0, j = 0; i <= s.size(); ++i) {
-            if (i == s.size() || s[i] == ' ') {
-                if (i - j > 2)
-                    s[j] = toupper(s[j]);
-                j = i + 1;
+    string capitalizeTitle(string title) {
+        for (int i = 0, prev=0; i <= title.size(); ++i) {
+            if (i == title.size() || title[i] == ' ') {
+                if (i - prev > 2) title[prev] = toupper(title[prev]);
+                prev = i + 1;
+            }else{
+                title[i] = tolower(title[i]);
             }
-            else
-                s[i] = tolower(s[i]);
         }
-        return s;
+        return title;
     }
 };
