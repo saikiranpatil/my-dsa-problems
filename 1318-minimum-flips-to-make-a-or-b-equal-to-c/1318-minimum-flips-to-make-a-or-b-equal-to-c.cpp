e586579291665d694e3acaf2ab49426c54b5d77c
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-07 07:54:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.28%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -4,8 +4,13 @@ public:
         int ans=0;
         
         while(a || b || c){
-            bool a_lsb = a%2, b_lsb = b%2, c_lsb = c%2;
-            a/=2, b/=2, c/=2;
+            bool a_lsb = a%2;
+            bool b_lsb = b%2;
+            bool c_lsb = c%2;
+            
+            a/=2;
+            b/=2;
+            c/=2;
             
             if((a_lsb || b_lsb) == c_lsb) continue;
             if(a_lsb == true && b_lsb == true) ans++;
