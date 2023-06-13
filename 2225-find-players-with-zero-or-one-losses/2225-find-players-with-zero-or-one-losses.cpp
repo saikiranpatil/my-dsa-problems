66ce597bb628b6161d6666be1439cb5bb5207220
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-13 20:08:22 +0530

    Time: 594 ms (93.09%), Space: 147.9 MB (99.07%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
+    int won[100001] = {0};
+    int lostCount[100001] = {0};
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         vector<vector<int>> ans(2);
         
-        unordered_map<int, int> lostCount;
-        unordered_set<int> won;
-        
         for(auto match: matches){
+            won[match[0]] = 1;
             lostCount[match[1]]++;
-            won.insert(match[0]);
         }
         
-        for(auto match: won){
-            if(lostCount[match] == 0){
-                ans[0].push_back(match);
+        for(int i=0;i<100001;i++){
+            if(won[i] == 1 && lostCount[i] == 0){
+                ans[0].push_back(i);
+            }else if(lostCount[i] == 1){
+                ans[1].push_back(i);
             }
         }
-        
-        for(auto lost: lostCount){
-            if(lost.second == 1){
-                ans[1].push_back(lost.first);
-            }
-        }
-        
-        sort(ans[0].begin(), ans[0].end());
-        sort(ans[1].begin(), ans[1].end());
         
         return ans;
     }
