9e7d7ce6daa48ec467fdba42e143cf8f9115702f
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-16 12:20:52 +0530

    Time: 878 ms (75.22%), Space: 7.8 MB (99.70%) - LeetHub

---
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countLatticePoints(vector<vector<int>>& circles) {
+        int n = circles.size();
+        int ans = 0;
+        
+        for(int x=0;x<=200;x++){
+            for(int y=0;y<=200;y++){
+                for(int i=0;i<n;i++){
+                    int a=circles[i][0],b=circles[i][1],r=circles[i][2];
+                    if((a-x)*(a-x) + (b-y)*(b-y) <= r*r){
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
