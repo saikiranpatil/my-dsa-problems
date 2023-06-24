2e44982f40dc820b878c51a3430aa579c66dfe6b
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-24 19:38:47 +0530

    Time: 9 ms (54.60%), Space: 6.5 MB (92.68%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
-    string add_string(string num1, string num2){
-        if(num1 == "") return num2;
-        if(num2 == "") return num1;
-        int m = num1.size(), n = num2.size(), carry = 0, curr;
-        string ans = "";
-        for(int i=0;i<max(m, n) || carry;i++){
-            curr = 0;
-            if(i<m) curr += num1[m-i-1] - '0';
-            if(i<n) curr += num2[n-i-1] - '0';
-            curr += carry;
-            carry = curr/10;
-            curr %= 10;
-            ans = to_string(curr) + ans;
-        }
-        return ans;
-    }
-    
     string multiply(string num1, string num2) {
         if(num1 == "0" || num2 == "0") return "0";
-        int m=num1.size(), n=num2.size();
-        string ans = "";
-        for(int i=m-1;i>=0;i--){
-            string curr = "";
-            int carry = 0, num;
-            for(int j=n-1;j>=0;j--){
-                num = (num1[i] - '0') * (num2[j] - '0');
-                num += carry;
-                carry = num/10;
-                num%=10;
-                curr = to_string(num) + curr;
+        int m = num1.size(), n=num2.size();
+        string ans(m+n, '0');
+        for(int i = m-1; i >= 0; i--) {
+            for(int j = n-1; j >= 0; j--) {
+                int curr = (ans[i+j+1]-'0') + (num1[i]-'0') * (num2[j]-'0');
+                ans[i+j+1] = curr%10 + '0';
+                ans[i+j] += curr/10;
             }
-            if(carry != 0) curr = to_string(carry) + curr;
-            curr += string(m-i-1, '0');
-            ans = add_string(ans, curr);
         }
+        if(ans[0] == '0') return ans.substr(1);
         return ans;
     }
 };
