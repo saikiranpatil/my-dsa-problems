151e1c57470599cedb47386872bff18a81716dc9
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-27 20:06:54 +0530

    Time: 201 ms (99.85%), Space: 106 MB (98.19%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,29 +1,39 @@
-class Solution {
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        auto comp = [&nums1, &nums2] (const pair<int, int>& a, const pair<int, int>& b) {
-            return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];
-        };
-        
-        vector<vector<int>> ans;
-        ans.reserve(k);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
-        pq.push({0,0});
-
-        while(k-- > 0 && !pq.empty()) {
-            const auto [i1, i2] = pq.top();
-            pq.pop();
-
-            ans.push_back({nums1[i1], nums2[i2]});
-
-            if(i1 == 0 && i2 + 1 < nums2.size()) {
-                pq.push({i1, i2+1});
-            }
-            if(i1 + 1 < nums1.size()) {
-                pq.push({i1+1, i2});
-            }
+	std::vector<std::vector<int>> kSmallestPairs(std::vector<int>& nums1, std::vector<int>& nums2, int k)
+	{
+		if (nums1.empty() || nums2.empty() || k <= 0) {
+            return {};
         }
-
-        return ans;
-    }
+		auto comp = [&nums1, &nums2](auto const &lhs, auto const &rhs)
+		{
+            auto &[li, lj] = lhs;
+            auto &[ri, rj] = rhs;
+			return nums1[li] + nums2[lj] > nums1[ri] + nums2[rj];
+		};
+		priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> heap(comp);
+		heap.emplace(0, 0);
+		
+		vector<vector<int>> result;
+		while (k-- && heap.size()) {
+			auto [i, j] = heap.top();
+			heap.pop();
+			result.push_back({nums1[i], nums2[j]});
+			if (i+1 < nums1.size()) {
+				heap.emplace(i+1, j);
+			}
+			if (i == 0 && j + 1 < nums2.size()) {
+				heap.emplace(i, j+1);
+			}
+		}
+		return result;
+	}
 };
