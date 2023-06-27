5e7d067a3c6bc55bd847d248f4d31d00f347cf06
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-27 20:05:01 +0530

    Time: 241 ms (97.90%), Space: 101.8 MB (99.41%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,24 +1,29 @@
 class Solution {
-public:    
+public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        auto customComparator = [&](const vector<int>& a, const vector<int>& b){
-            return a[0] + a[1] > b[0] + b[1];    
+        auto comp = [&nums1, &nums2] (const pair<int, int>& a, const pair<int, int>& b) {
+            return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];
         };
         
         vector<vector<int>> ans;
-        priority_queue<vector<int>, vector<vector<int>>, decltype(customComparator)> pq(customComparator);
-        for(int i=0;i<nums1.size();i++){
-            pq.push({nums1[i], nums2[0], 0});
-        }
-        while(k-- && !pq.empty()){
-            vector<int> curr = pq.top();
+        ans.reserve(k);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+        pq.push({0,0});
+
+        while(k-- > 0 && !pq.empty()) {
+            const auto [i1, i2] = pq.top();
             pq.pop();
-            ans.push_back({curr[0], curr[1]});
-            
-            if(curr[2]!=nums2.size()-1){
-                pq.push({curr[0], nums2[curr[2]+1], curr[2]+1});
+
+            ans.push_back({nums1[i1], nums2[i2]});
+
+            if(i1 == 0 && i2 + 1 < nums2.size()) {
+                pq.push({i1, i2+1});
+            }
+            if(i1 + 1 < nums1.size()) {
+                pq.push({i1+1, i2});
             }
         }
+
         return ans;
     }
 };
