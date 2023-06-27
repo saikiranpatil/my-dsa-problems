4fe97b464d46cf242f2bdebd0b61f4712813396e
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-27 23:45:49 +0530

    Time: 9 ms (89.95%), Space: 7 MB (95.13%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,11 +3,9 @@ public:
     int change(int amount, vector<int>& coins) {
         vector<int> dp(amount + 1, 0);
         dp[0] = 1;
-        for(int i : coins)
-        {
-            for(int j = i; j <= amount; j++)
-            {
-                dp[j] += dp[j - i];
+        for(int i=0;i<coins.size();i++){
+            for(int j = coins[i]; j <= amount; j++){
+                dp[j] += dp[j - coins[i]];
             }
         }
         return dp[amount];
