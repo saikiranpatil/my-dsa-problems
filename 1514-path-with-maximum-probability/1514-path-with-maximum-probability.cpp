9505a1a1beaca9bf75c4c6829d635337954b504a
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-28 22:47:15 +0530

    Time: 173 ms (98.91%), Space: 49.3 MB (98.76%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<int,double>>> graph(n);
-        for(int i=0;i<edges.size();i++){
-            int x=edges[i][0];
-            int y=edges[i][1];
-            graph[x].push_back({y,succProb[i]});
-            graph[y].push_back({x,succProb[i]});
-        }
-        vector<double> nums(n,0.0);
-        nums[start]=1.0;
-        queue<int> q;
-        q.push(start);
-
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-            for(auto it:graph[curr]){
-                double currNode=it.first;
-                double currProb=it.second;
-                double newProb=nums[curr]*currProb;
-                if(newProb>nums[currNode]){
-                    nums[currNode]=newProb;
-                    q.push(currNode);
+        vector<double>dist(n,0);
+        dist[start] = 1;
+        for(int i = 0; i < n - 1; i++){
+            bool hasUpdate = false;
+            for(int j = 0; j < edges.size(); j++){
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
+                if(dist[u] * prob > dist[v]){
+                    dist[v] = dist[u] * prob;
+                    hasUpdate = true;
+                }
+                if(dist[v] * prob > dist[u]){
+                    dist[u] = dist[v] * prob;
+                    hasUpdate = true;
                 }
             }
+            if(!hasUpdate){
+                break;
+            }
         }
-        return nums[end];
+        return dist[end];
     }
 };
