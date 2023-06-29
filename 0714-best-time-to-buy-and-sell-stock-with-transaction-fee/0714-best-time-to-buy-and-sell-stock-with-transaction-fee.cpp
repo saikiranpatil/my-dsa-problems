db72c12c3a3d40dd88561d520d52c2fd2e8141fd
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-29 10:14:14 +0530

    Time: 111 ms (78.97%), Space: 54.9 MB (79.82%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-    int dp[50001][2];
-    int price;
-    int helper(vector<int>& prices, int idx, int isBuy){
-        if(idx>=prices.size()){
-            return 0;
-        }
-        if(dp[idx][isBuy]!=-1){
-            return dp[idx][isBuy];
-        }
-        if(isBuy){
-            return dp[idx][isBuy] = max(helper(prices, idx+1, 0) - prices[idx], helper(prices, idx+1, 1));
-        }
-        return dp[idx][isBuy] = max(helper(prices, idx+1, 1) + prices[idx] - price, helper(prices, idx+1, 0));
-    }
     int maxProfit(vector<int>& prices, int fee) {
-        memset(dp, -1, sizeof(dp));
-        price=fee;
-        return helper(prices, 0, 1);
+        int buy = INT_MIN, sell = 0;
+        for (int price : prices) {
+            buy = max(buy, sell - price);
+            sell = max(sell, buy + price - fee);
+        }
+        return sell;
     }
 };
