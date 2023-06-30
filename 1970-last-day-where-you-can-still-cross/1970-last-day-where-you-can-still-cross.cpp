80b5deb6c7081336d9040b1a6b8de04574421abf
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-30 14:33:54 +0530

    Time: 347 ms (87.39%), Space: 99.6 MB (93.24%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,45 +1,61 @@
 class Solution {
 public:
-    bool dfs(int x, int y, int row, int col, vector<vector<bool>>& visited) {
-        if (x < 0 || x >= row || y < 0 || y >= col || visited[x][y]) {
-            return false;
+    static const int maxN=20004;
+    int p[maxN],size[maxN];
+    void make_set(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            p[i]=i;
+            size[i]=1;
         }
-
-        if (x == row - 1) {
-            return true;
-        }
-
-        visited[x][y] = true;
-
-        return dfs(x + 1, y, row, col, visited) || dfs(x - 1, y, row, col, visited) ||
-               dfs(x, y + 1, row, col, visited) || dfs(x, y - 1, row, col, visited);
     }
-
-    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        auto isValid = [&](int mid) {
-            vector<vector<bool>> visited(row, vector<bool>(col, false));
-            for (int i = 0; i <= mid; i++) {
-                visited[cells[i][0] - 1][cells[i][1] - 1] = true;
-            }
-
-            for (int j = 0; j < col; j++) {
-                if (dfs(0, j, row, col, visited)) {
-                    return true;
+    int find(int a)
+    {
+        if(a==p[a]) return a;
+        return p[a]=find(p[a]);
+    }
+    void Union(int a,int b)
+    {
+        a=find(a);
+        b=find(b);
+        if(a!=b)
+        {
+            if(size[b]>size[a]) swap(a,b);
+            p[b]=a;
+            size[a]+=size[b];
+        }
+    }
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) 
+    {
+        int grid[row][col];
+        memset(grid,0,sizeof(grid));
+        int top=row*col,bottom=row*col+1;
+        p[top]=top,p[bottom]=bottom;
+        size[top]=size[bottom]=1;
+        make_set(row*col);
+        for(int i=cells.size()-1;i>=0;i--)
+        {
+            int x=cells[i][0]-1,y=cells[i][1]-1;
+            int idx1=x*col+y;
+            grid[x][y]=1;
+            int dx[]={0,-1,0,1};
+            int dy[]={1,0,-1,0};
+            for(int k=0;k<4;k++)
+            {
+                int nx=x+dx[k],ny=y+dy[k];
+                if(nx>=0&&nx<row&&ny>=0&&ny<col&&grid[nx][ny]==1)
+                {
+                    int idx2=nx*col+ny;
+                    Union(idx1,idx2);
+                    if(nx==0) Union(idx2,top);
+                    if(nx==row-1) Union(idx2,bottom);
                 }
             }
-
-            return false;
-        };
-
-        int low = 0, high = cells.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isValid(mid)) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            if(x==0) Union(idx1,top);
+            if(x==row-1) Union(idx1,bottom);
+            if(find(top)==find(bottom)) return i;
         }
-        return low;
+        return -1;
     }
 };
