57335c0bb1712fc1346c6180610f853f344c5565
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-30 17:37:15 +0530

    Time: 15 ms (75.06%), Space: 13.6 MB (8.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<bool> row(m, false);
-        vector<bool> col(n, false);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int r=matrix.size();
+        int c=matrix[0].size();
+        vector<vector<int>> visit = matrix;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
                 if(matrix[i][j] == 0){
-                    row[i]=true;
-                    col[j]=true;
+                    for(int k=0; k<c; k++){
+                        visit[i][k] = 0;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i] == true || col[j] == true){
-                    matrix[i][j]=0;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<r; k++){
+                        visit[k][j] = 0;
+                    }
                 }
+            }
+        }
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                matrix[i][j] = visit[i][j];
             }
         }
     }
