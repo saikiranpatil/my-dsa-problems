a73e560f2f2348f903d9a77c24c890ddf2051d8a
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-06-30 09:57:43 +0530

    Time: 3 ms (53.95%), Space: 6.7 MB (11.44%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        string ans = "";
-        for(int i=0;i<num.size()-2;i++){
-            if(num[i]==num[i+1] && num[i+1]==num[i+2]){
-                if(ans.size() == 0 || num[i]>ans[0]){
-                    ans = num.substr(i, 3);
-                }
+        vector<int> v;
+        for (int i = 0; i < num.size() - 2; i++) {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+                v.push_back(num[i] - '0');
+                v.push_back(num[i + 1] - '0');
+                v.push_back(num[i + 2] - '0');
+                i += 2;
             }
         }
-        return ans;
+        sort(v.begin(), v.end());
+        if (v.empty()) {
+            return "";
+        } else {
+            string result;
+            result.push_back(v[v.size() - 1] + '0');
+            result.push_back(v[v.size() - 2] + '0');
+            result.push_back(v[v.size() - 3] + '0');
+            return result;
+        }
     }
 };
