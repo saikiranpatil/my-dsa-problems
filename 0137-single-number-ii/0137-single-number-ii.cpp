6f6cbf6e16c76ec79ef44ef4611b3cd725422c48
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-04 11:46:33 +0530

    Time: 33 ms (6.10%), Space: 9.5 MB (72.62%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        sort(nums.begin(), nums.end());
+        for(auto num: nums){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        for(int i=1;i<nums.size();i+=3){
+            if(nums[i-1]!=nums[i]) return nums[i-1];
+            if(nums[i+1]!=nums[i]) return nums[i+1];
+        }
+        return -1;
+    }
+};
