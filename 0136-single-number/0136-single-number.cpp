755890e0d5418287d150f57acbc19b94d4fbd466
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-04 09:50:02 +0530

    Time: 18 ms (80.54%), Space: 16.9 MB (67.03%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,6 +3,11 @@ public:
     int singleNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+        
+        int ans=0;
+        for(auto num:nums){
+            ans^=num;
+        }
+        return ans;
     }
 };
