577e79411c7de321d7b7298700b8f5ec89f9f33d
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-04 20:16:21 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (71.20%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helper(vector<int>& dp, int n){
+        if(dp[n]!=-1) return dp[n];
+        if(n<2) return dp[n] = 1;
+        int ans = n;
+        for(int i=1;i<=n;i++){
+            ans = max(ans, i*helper(dp, n-i));
+        }
+        return dp[n] = ans;
+    }
+    int integerBreak(int n){
+        if(n<2) return 1;
+        if(n<4) return n-1;
+        
+        vector<int> dp(n+1, -1);
+        return helper(dp, n);
+    }
+};
