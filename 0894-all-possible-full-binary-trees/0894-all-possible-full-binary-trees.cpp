fd4fc014eb15dafc1af36ecb97c044aad78cfc95
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-04 16:53:45 +0530

    Time: 97 ms (82.23%), Space: 28 MB (78.15%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -11,34 +11,37 @@
  */
 class Solution {
 public:
-    unordered_map<int, vector<TreeNode*>> memo;
-    vector<TreeNode*> allPossibleFBT(int n) {
-        if ((n % 2) == 0) {
+    map<int, vector<TreeNode *>> memo;
+    vector<TreeNode *> allPossibleFBT(int n){
+        if (!(n % 2))
             return {};
-        }
-        
-        if(n==1){
-            return {new TreeNode()};
-        }
-        
-        if(memo.find(n) != memo.end()) {
+
+        if (n == 1)
+            return {new TreeNode(0)};
+
+        if (memo.find(n) != memo.end())
             return memo[n];
-        }
-        
-        vector<TreeNode*> ans;
-        
-        for(int i=1;i<n;i+=2){
-            vector<TreeNode*> lefts = allPossibleFBT(i);
-            vector<TreeNode*> rights = allPossibleFBT(n-i-1);
-            
-            for(auto left:lefts){
-                for(auto right:rights){
-                    TreeNode* curr = new TreeNode(0, left, right);
-                    ans.push_back(curr);
+
+        vector<TreeNode *> result;
+
+        for (int i = 1; i < n; i += 2)
+        {
+            vector<TreeNode *> left = allPossibleFBT(i);
+            vector<TreeNode *> right = allPossibleFBT(n - i - 1);
+
+            for (auto l : left)
+            {
+                for (auto r : right)
+                {
+                    TreeNode *root = new TreeNode(0);
+                    root->left = l;
+                    root->right = r;
+                    result.push_back(root);
                 }
             }
         }
-        
-        return memo[n] = ans;
+
+        memo[n] = result;
+        return result;
     }
 };
