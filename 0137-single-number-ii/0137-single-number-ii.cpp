f8758d07e6af6c4510bafc7a53bb184dfaadaaad
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-04 11:46:43 +0530

    Time: 13 ms (28.44%), Space: 9.5 MB (41.15%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -4,10 +4,6 @@ public:
         int n = nums.size();
         if(n==1) return nums[0];
         sort(nums.begin(), nums.end());
-        for(auto num: nums){
-            cout<<num<<" ";
-        }
-        cout<<endl;
         if(nums[0]!=nums[1]) return nums[0];
         if(nums[n-1]!=nums[n-2]) return nums[n-1];
         for(int i=1;i<nums.size();i+=3){
