5ef3dd77640449e361f50d44f91a770d9058d0af
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-05 11:39:14 +0530

    Time: 5 ms (65.41%), Space: 7.4 MB (81.07%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
-    int memo[501][501];
-    
-    int helper(vector<int>& piles, int start, int end, int diff) {
-        if (start > end) {
-            return diff > 0;
-        }
-        
-        if (memo[start][end] != 0) {
-            return memo[start][end];
-        }
-        
-        return memo[start][end] = max(piles[start] - helper(piles, start + 1, end, -diff + piles[start]),
-                                      piles[end] - helper(piles, start, end - 1, -diff + piles[end]));
-    }
-    
     bool stoneGame(vector<int>& piles) {
-        return helper(piles, 0, piles.size() - 1, 0) > 0;
+        return true;
     }
 };
