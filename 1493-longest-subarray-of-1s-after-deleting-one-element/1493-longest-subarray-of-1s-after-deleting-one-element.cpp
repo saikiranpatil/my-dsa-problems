6bf89f48c3cf363c7e25d0360baf0983179521d0
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-05 09:15:03 +0530

    Time: 38 ms (88.20%), Space: 36.5 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        
-        int n=nums.size(), left=-1, ans = 0, sum=0, curr=0;
+        int n=nums.size(), left=-1, ans = 0, curr=0;
         for(int right=0;right<n;right++){
             curr += nums[right];
             while(curr<right-left-1){
                 curr -= nums[++left];
             }
             ans = max(ans, curr);
-            sum+=nums[right];
         }
-        return sum == n ? n-1 : ans;
+        return ans == n ? n-1 : ans;
     }
 };
