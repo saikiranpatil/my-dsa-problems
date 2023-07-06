9b23d4e27e57e49a20fa1b4c2905e77728c5cb5d
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-06 10:33:08 +0530

    Time: 49 ms (26.47%), Space: 28.8 MB (5.79%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool checker(vector<int>& nums, int len, int n, int target){
-        int sum = 0;
-        
-        for(int i=0;i<len;i++){
-            sum += nums[i];
-        }
-        
-        for(int i=len;i<n;i++){
-            if(sum>=target){
-                return true;
-            }
-            sum+=nums[i];
-            sum-=nums[i-len];
-        }
-        
-        return sum>=target;
-    }
     int minSubArrayLen(int target, vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0), n = nums.size();
         if(target > sum) return 0;
         if(target == sum) return n;
         
+        auto checker = [nums, n, target](int len){
+            int sum = 0;
+        
+            for(int i=0;i<len;i++){
+                sum += nums[i];
+            }
+
+            for(int i=len;i<n;i++){
+                if(sum>=target){
+                    return true;
+                }
+                sum+=nums[i];
+                sum-=nums[i-len];
+            }
+
+            return sum>=target;
+        };
+        
         int low = 1, high = n-1, mid;
         while(low<=high){
             mid = low + (high - low) / 2;
-            if(checker(nums, mid, n, target)){
+            if(checker(mid)){
                 high = mid-1;
             }else{
                 low = mid+1;
