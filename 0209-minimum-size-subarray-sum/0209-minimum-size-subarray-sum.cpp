8cc3616216a5c927a456216f18641432983b1f1a
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-06 11:56:55 +0530

    Time: 46 ms (39.99%), Space: 28.1 MB (99.58%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,41 +1,14 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        int sum = accumulate(nums.begin(), nums.end(), 0), n = nums.size();
-        if(target > sum) return 0;
-        if(target == sum) return n;
-        
-        auto checker = [nums, n, target](int len){
-            int sum = 0;
-        
-            for(int i=0;i<len;i++){
-                sum += nums[i];
-            }
-
-            for(int i=len;i<n;i++){
-                if(sum>=target){
-                    return true;
-                }
-                sum+=nums[i];
-                sum-=nums[i-len];
-            }
-
-            return sum>=target;
-        };
-        
-        int low = 1, high = n-1, mid;
-        while(low<=high){
-            mid = low + (high - low) / 2;
-            if(checker(mid)){
-                high = mid-1;
-            }else{
-                low = mid+1;
+        int n=nums.size(), sum=0, left = -1, ans=INT_MAX;
+        for(int right=0;right<n;right++){
+            sum+=nums[right];
+            while(sum>=target){
+                sum-=nums[++left];
+                ans=min(ans, right-left+1);
             }
         }
-        
-        return low;
+        return ans == INT_MAX ? 0 : ans;
     }
 };
