e2bc44e09c35a4f34241134cf9c3284c3d89deaf
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-08 13:37:27 +0530

    Time: 166 ms (63.14%), Space: 57.6 MB (22.57%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         vector<int> count(24, 0);
-        int n = candidates.size();
         for(int i=0;i<24;i++){
-            for(int j=0;j<n;j++){
-                count[i]+=(candidates[j]>>i & 1);
+            for(auto num: candidates){
+                count[i]+=(num>>i & 1);
             }   
         }
         return *max_element(begin(count), end(count));
