f4d68513196764c49cca811a33f61cccc895a817
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-08 11:50:32 +0530

    Time: 4 ms (97.76%), Space: 12.3 MB (75.73%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        auto sort_str = [](string str){
-            sort(begin(str), end(str));
-            return str;
-        };
-        
-        vector<string> ans;
-        int n = words.size();
-        for(int i=0;i<n;i++){
-            ans.push_back(words[i]);
-            string curr = sort_str(words[i]);
-            while(i+1<n && sort_str(words[i+1]) == curr){
-                i++;
+       vector<string> ans;
+        string prev = "";
+        for (auto word : words)
+        {
+            string temp = word;
+            sort(temp.begin(), temp.end());
+            if (temp != prev)
+            {
+                ans.push_back(word);
             }
+            prev = temp;
         }
         return ans;
     }
