47c4bd6d841e298bf7118a4872855502c7022f50
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-08 23:51:36 +0530

    Time: 419 ms (16.67%), Space: 185.4 MB (16.67%) - LeetHub

--- 2766-relocate-marbles/2766-relocate-marbles.cpp
+++ 2766-relocate-marbles/2766-relocate-marbles.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
-        int n = moveFrom.size();
-        vector<int> ans;
-        map<int, long> count;
-        for(auto num: nums){
-            count[num]++;
+        set<int> s(begin(nums), end(nums));
+        for (int i = 0; i < moveFrom.size(); ++i) {
+            s.erase(s.find(moveFrom[i]));
+            s.insert(moveTo[i]);
         }
-        
-        for(int i=0;i<n;i++){
-            count[moveTo[i]] += count[moveFrom[i]];
-            if(moveFrom[i] != moveTo[i]) count.erase(moveFrom[i]);
-        }
-        
-        for(auto num: count){
-            ans.push_back(num.first);
-        }
-        
-        return ans;
+        return vector<int>(begin(s), end(s));
     }
 };
