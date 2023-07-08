3d5876d4c3b97c9841a26fafd9f6be16720a6ce8
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2023-07-08 11:52:12 +0530

    Time: 8 ms (86.92%), Space: 12 MB (100.00%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
         vector<string> ans;
         string prev = "";
         int n = words.size();
+        
         for (int i=0;i<n;i++){
             string temp = words[i];
             sort(temp.begin(), temp.end());
@@ -15,6 +13,7 @@ public:
             }
             prev = temp;
         }
+        
         return ans;
     }
 };
